b70b7b7fceafa5c092b66a8aa4a5adb97ec2df0c
devkow77 <kowalsky429@gmail.com>
2025-11-27 16:18:20 +0100

    Movie Details

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,14 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 text-center font-bold">
-        Profile
-      </Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
     </View>
   );
 };
